a65443bda1c8d21cabcb7574d564708938b9c725
jrcharlo <jrcharlo@buffalo.edu>
2017-04-13 16:05:39 -0400

    added trilateration implementation

--- java/tools/Listen.java
+++ java/tools/Listen.java
@@ -7,8 +7,9 @@ import java.lang.Math.*;
 public class Listen {
   static int numnodes = 5;
   static int numrelaynodes = numnodes - 2;
-  static double[] distances = new double[numrelaynodes];
-  static double[][] relaynodes = new double[numrelaynodes][2];
+  static double[] tdistances = new double[numrelaynodes]; //target distances
+  static int[] targetd = new int[3]; // 3 closest nodes to target
+  static double[][] rnodes = new double[numrelaynodes][2];
 
 
   public static void main(String args[]) throws IOException {
@@ -74,31 +75,64 @@ public class Listen {
   }
 
   public static void initializeNodes(){
-    relaynodes[0][0] = 0; // node's x position
-    relaynodes[0][1] = 0; // node's y position
-    relaynodes[1][0] = 45; // node's x position
-    relaynodes[1][1] = 90; // node's y position
-    relaynodes[2][0] = 90; // node's x position
-    relaynodes[2][1] = 0; // node's y position
+    rnodes[0][0] = 0; // node's x position
+    rnodes[0][1] = 0; // node's y position
+    rnodes[1][0] = 45; // node's x position
+    rnodes[1][1] = 90; // node's y position
+    rnodes[2][0] = 90; // node's x position
+    rnodes[2][1] = 0; // node's y position
   }
 
   public static void updateNode(int nodeid, double distance){
-    distances[nodeid-numrelaynodes] = distance;
+    tdistances[nodeid-numrelaynodes] = distance;
     if(nodeid == numnodes){
-      locate();
+      locateTarget();
     }
   }
 
-  public static void locate(){
-//    for(int i = 0; i < distances.length; i++){
-//      System.out.println("Node " + (int)(i+numrelaynodes) + " is " + distances[i] + " m away from the target.");
+  public static void locateTarget(){
+//    for(int i = 0; i < tdistances.length; i++){
+//      System.out.println("Node " + (int)(i+numrelaynodes) + " is " + tdistances[i] + " m away from the target.");
 //    }
     /*
     Node 1 at (0,0), Node 2 at (d, 0), Node 3 at (i, j) (i along x axis, j along y axis from Node 1)
     x = (r1^2 - r2^2 + d^2)/(2*d)
-    y = (r1^2 - r3^2 + i^2 + j^2)/(2*j) - (i*x)/j
+    y = (r1^2 - r3^2 + j^2 + k^2)/(2*k) - (j*x)/k
+    use distance equation to figure out d, j, k
     */
+    calculateSmallest(0);
+    int n1 = targetd[0];
+    int n2 = targetd[1];
+    int n3 = targetd[2];
+    double d12 = 0;
+    // d12 = sqrt((x1-x2)^2 + (y1-y2)^2)
+    d12 = Math.sqrt(Math.pow(Math.abs(rnodes[n1][0] - rnodes[n2][0]), 2) + Math.pow(Math.abs(rnodes[n1][1] - rnodes[n2][1]), 2));
+    int targetx = 0;
+    targetx = (Math.pow(tdistances[n1], 2) - Math.pow(tdistances[n2], 2) + Math.pow(d12, 2))/(2*d12)
+    double d13 = 0;
+    d13 = Math.sqrt(Math.pow(Math.abs(rnodes[n1][0] - rnodes[n3][0]), 2) + Math.pow(Math.abs(rnodes[n1][1] - rnodes[n3][1]), 2));
+    double angle13 = 0;
+    angle13 = Math.atan(Math.abs(rnodes[n1][0] - rnodes[n3][0])/Math.abs(rnodes[n1][1] - rnodes[n3][1]));
+    double j = 0;
+    double k = 0;
+    j = d13*Math.cos(angle13);
+    k = d13*Math.sin(angle13);
+    int targety = 0;
+    targety = (Math.pow(tdistances[n1],2) - Math.pow(tdistances[n3],2) + Math.pow(j,2) + Math.pow(k,2))/(2*k) - (j*targetx)/k
+    System.out.println("Target is at (x,y) = ("+targetx+", "+targety+").");
+  }
 
+  public static void calculateSmallest(int case){
+    if(case == 0){
+      for(int i = 0; i < 3; i++){ // stupid scheme that works because there are 3 nodes
+        // this should ideal place the index of the smallest distance in tdistances at 0
+        // second smallest at 1, largest at 2
+        targetd[i] = [i];
+      }
+    }
+    else if(case == 1){
+      // should calculate things for car distances here or something
+    }
   }
 
 }
