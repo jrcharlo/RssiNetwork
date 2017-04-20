f81128f52b2e5db034828a551d048566df20ee02
jrcharlo <jrcharlo@buffalo.edu>
2017-04-20 11:54:51 -0400

    will  now be easier to debug

--- java/tools/Listen.java
+++ java/tools/Listen.java
@@ -130,9 +130,9 @@ public class Listen {
 
   public static void calculateSmallest(int mcase){
     if(mcase == 0){ //tnodes (target nodes)
-      tnodes[0] = 7;
-      tnodes[1] = 7;
-      tnodes[2] = 7;
+      tnodes[0] = 99; // 99's should get phased out as real data spills in, set to 99 for debugging
+      tnodes[1] = 99;
+      tnodes[2] = 99;
       double min1 = 9997;
       double min2 = 9998;
       double min3 = 9999;
