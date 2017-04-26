4c2099421a333d6291e5df852640e6f5ac950e38
jrcharlo <jrcharlo@buffalo.edu>
2017-04-26 19:47:43 -0400

    can now communicate to car via sockets

--- java/Grid.java
+++ java/Grid.java
@@ -16,8 +16,8 @@ public class Grid{
   private Boolean cReady;
   private double carx;
   private double cary;
+  private String carIP;
   private Socket carSocket;
-  private OutputStreamWriter osw;
 
   public Grid(int nn, int nnrn, int nrn){
     numNodes = nn;
@@ -27,23 +27,14 @@ public class Grid{
     tnodes = new int[3];
     cnodes = new int[3];
     tReady = false;
-    targetx = 0;
-    targety = 0;
+    targetx = 9.0;
+    targety = 0.57;
     cReady = false;
-    carx = 0;
-    cary = 0;
+    carx = 8.02;
+    cary = 70.0;
     initializeNodes();
-    String carIP = "192.168.4.1";
-    try{
-      carSocket = new Socket();//carIP, 8989);
-      carSocket.connect(new InetSocketAddress(carIP, 8989), 5000);
-      osw = new OutputStreamWriter(carSocket.getOutputStream(), "UTF-8");
-    }
-    catch (Exception e){
-      e.printStackTrace();
-    }
-    sendtoCar();
-    System.exit(-1);
+    carIP = "192.168.4.1";
+    sendtoCar(); // debug purposes
   }
 
   /*
@@ -220,15 +211,20 @@ public class Grid{
 
   public void sendtoCar(){
     //send stuff to car here using socket
-    System.out.println("Sending msg to car!");
-    String s = "GET /Hello car\r Hello???";
     try{
-      osw.write(s, 0, s.length());
+      carSocket = new Socket();
+      carSocket.connect(new InetSocketAddress(carIP, 8989), 5000);
+      DataOutputStream dos = new DataOutputStream(carSocket.getOutputStream());
+
+      String coords = carx+" "+cary+" "+targetx+" "+targety+" "; //null terminating " " is important! car relies on it
+      dos.writeBytes(coords);
+
+      carSocket.close();
     }
-    catch(Exception e){
+    catch (Exception e){
       e.printStackTrace();
     }
-    System.out.println("Msg sent!");
+
     for(int i = 0; i < rnodes.length; i++){
       rnodes[i].td = 9999; // reset all distances
       rnodes[i].cd = 9999; // reset all distances
